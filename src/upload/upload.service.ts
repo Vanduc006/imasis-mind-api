68bd003c245debfe29f8ccf093861ebf933d511e
Nguyen Van Duc <98450746+Vanduc006@users.noreply.github.com>
2025-04-16 06:43:18 +0000

    Pending changes exported from your codespace

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import * as pdf from 'pdf-parse';
+import { v2 as cloudinary } from 'cloudinary'; 
+import 'dotenv/config'
 @Injectable()
-export class UploadService {}
+
+export class UploadService { 
+    async transferCloud(file : Express.Multer.File) {
+        cloudinary.config({ 
+            cloud_name: process.env.HELLO, 
+            api_key: 'my_key', 
+            api_secret: 'my_secret'
+        });
+    }
+    async PrasePDF(file : Express.Multer.File) {
+        try {
+            const data = await pdf(file.buffer); 
+            // console.log(data.info)
+            return data;
+            
+          } catch (error) {
+            console.error('PDF parse error:', error);
+            return error;
+          }
+    }
+}
