08054243fa52b07b495c1e3e385be5172030bd55
Nguyen Van Duc <98450746+Vanduc006@users.noreply.github.com>
2025-04-28 08:30:28 +0000

    Pending changes exported from your codespace

--- src/llm/llm.controller.ts
+++ src/llm/llm.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, Sse, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { LlmService } from './llm.service';
 import { TransformfileService } from 'src/transformfile/transformfile.service';
 // import { ENV } from 'config/env';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
+import { Subject } from 'rxjs';
+import { Response } from 'express';
+
 @Controller('llm')
 export class LlmController {
+    private readonly progressSuject = new Subject<any>()
     constructor(
         private readonly LlmService : LlmService,
         private readonly TransformfileService : TransformfileService,
@@ -18,16 +22,72 @@ export class LlmController {
         })
     )
     async createEmbedding(@UploadedFile() file: Express.Multer.File) {
+        this.progressSuject.next({
+            data: {
+              score: 10,
+              description: '[MIND] Open file'
+            }
+          });
+
+        // 1 prase pdf
         const extract = await this.TransformfileService.PrasePDF(file)
-        // const extractContent = extract.originContent.text
+        this.progressSuject.next({
+            data: {
+              score: 10,
+              description: '[MIND] Parse PDF'
+            }
+          });
+        
+        const chunks = await this.LlmService.langchainChunk(extract)
+          
+        // 2 moderation
         const moderation = await this.LlmService.moderationGPT(extract)
+        this.progressSuject.next({
+            data: {
+              score: 10,
+              description: '[MIND] Understading content'
+            }
+          });
+
+        // 3 embedding
         const embedding = await this.LlmService.geminiEmbedding(extract)
+        this.progressSuject.next({
+            data: {
+              score: 10,
+              description: '[MIND] Dive into context'
+            }
+          });
+
+        // 4 store vector
+        const storeVector = await this.LlmService.storeEmbedding(embedding,'123','pdf','1','test.pdf')
+        this.progressSuject.next({
+            data: {
+              score: 10,
+              description: '[MIND] Deepsearch'
+            }
+          });
         return ({
             extract : extract,
             embedding : embedding,
+            langchain : chunks,
             moderation : moderation.results[0].flagged,
+            qdrant : storeVector,
             // embedding : embedding,
         })
+
+    }
+    @Get('progress')
+    @Sse('progress') 
+    sendProgress(@Res() res: Response) {
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Nếu dùng CORS
+        setTimeout(() => {
+            this.progressSuject.complete();  // Đóng kết nối khi hoàn thành
+            res.end();  // Đảm bảo kết nối được đóng khi hoàn tất
+          }, 3000);
+        return this.progressSuject.asObservable()
     }
 
     // @Get('moderation')
--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -3,15 +3,23 @@ import OpenAI from 'openai';
 import { GoogleGenAI } from '@google/genai';
 import { QdrantClient } from '@qdrant/js-client-rest';
 import { ENV } from 'config/env';
-
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class LlmService {
 
     private readonly qdrantURL = ENV.QDRANT_URL
-    private readonly qdrantPORT = ENV.QDRANT_PORT
-    private readonly client = new QdrantClient({host : this.qdrantURL, port : this.qdrantPORT})
+    private readonly qdrantAPIKEY = ENV.QDRANT_API_KEY
+    
+    private readonly client = new QdrantClient({
+      url: this.qdrantURL,
+      apiKey: this.qdrantAPIKEY,
+      port : undefined,
+    });
+    
     private readonly clientGPT = new OpenAI()
+
     async embedding( prompt: string) {
         let specificPrompt = ""
         switch (prompt) {
@@ -26,7 +34,37 @@ export class LlmService {
         }
         return specificPrompt
     }
+
+    async langchainChunk(praseData : string ) {
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize : 500,
+            chunkOverlap : 100,
+
+        })
+        return await splitter.splitText(praseData)
+    }
     
+    // async geminiEmbedding(chunks: string[]) {
+    //     const ai = new GoogleGenAI({ apiKey: ENV.GEMINI_API_KEY });
+    
+    //     const embeddings: number[][] = [];
+    
+    //     for (const chunk of chunks) {
+    //         const response = await ai.models.embedContent({
+    //             model: 'gemini-embedding-exp-03-07',
+    //             contents: chunk,
+    //         });
+    //         if (response.embeddings?.[0]?.values) {
+    //             embeddings.push(response.embeddings[0].values);
+    //         }
+    //         // Nếu cần delay giữa các request, thêm sleep vào đây
+    //         await new Promise(resolve => setTimeout(resolve, 5000)); // 100ms delay
+    //     }
+    
+    //     return embeddings;
+    // }
+    
+
     async geminiEmbedding(parseData : string) {
         const ai = new GoogleGenAI({ apiKey : ENV.GEMINI_API_KEY})
         const respone = await ai.models.embedContent({
@@ -36,24 +74,34 @@ export class LlmService {
         return respone.embeddings?.[0].values
     }
 
-    async storeEmbedding(vector : number[],userID : string, collectionName : string, spaceID : string, fileID: string) {
-        const data = await this.client.upsert(collectionName, {
-            points : [
+    
+
+    // async gptEmbedding(praseData : string ) {
+
+    // }
+
+    async storeEmbedding(vector : any,userID : string, collectionName : string, spaceID : string, fileID: string) {
+        const promises = vector.map((vec, idx) => 
+            this.client.upsert(collectionName, {
+              points: [
                 {
-                    id : "chunk",
-                    vector : vector,
-                    payload : {
-                        spaceID : spaceID,
-                        userID: userID,
-                        fileID: fileID,
-                        chunkIndex: 3,
-                    }
+                  id: uuidv4(),
+                  vector: vec,
+                  payload: {
+                    spaceID: spaceID,
+                    userID: userID,
+                    fileID: fileID,
+                    chunkIndex: idx,
+                  }
                 }
-            ]
-        })
+              ]
+            })
+          );
 
-
+        const data = await Promise.all(promises);
+        return data
     }
+    
     // userID, spaceID, fileID, collectionName, vector
     async queryEmbedding(vector : number[],UserID: string, spaceID: string, fileID : string, collectionName: string) {
         const result = await this.client.search(collectionName, {
