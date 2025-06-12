203769ec6170b08a9c0e10ca3ab278a09d25a899
vanduc006 <bagiadaohoa@gmail.com>
2025-06-12 10:08:46 +0700

    remove port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
       'https://cuddly-chainsaw-q59gwg69w57h9w47-5173.app.github.dev',
       'https://imasis.id.vn',
       'https://glorious-meme-wpqg9v4x4662www-5173.app.github.dev',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1',
+      'http://localhost',
+      // 'http://127.0.0.1:5173',
+      // 'http://localhost:5173',
     ],
     credentials: true,
   });
